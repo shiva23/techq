066618556fc8af67a5ed033b2b65090625d42269
Shiv Keshri <spk12892@gmail.com>
2016-06-13 19:38:24 +0530

    do it again3

--- tiq/wsgi.py
+++ tiq/wsgi.py
@@ -23,11 +23,9 @@ application = DjangoWhiteNoise(application)
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from dj_static import Cling
+try:
+	from dj_static import Cling
 
-application = Cling(get_wsgi_application())
-
-
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-# application = Cling(get_wsgi_application())
+	application = Cling(get_wsgi_application())
+except:
+	pass
