18db49c20f4f714f131861f51ddc0d2f2935650c
Shiv Keshri <spk12892@gmail.com>
2016-06-13 20:00:38 +0530

    do it again6

--- tiq/urls.py
+++ tiq/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from question import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
